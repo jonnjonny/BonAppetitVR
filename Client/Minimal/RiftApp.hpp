756f2c4898452a9a6b5d2c76f52e68d920558d6b
Manqing Zhang <maz026@ucsd.edu>
2019-06-08 22:20:52 -0700

    wierd fixedgit add .git add .git add .

--- Client/Minimal/RiftApp.hpp
+++ Client/Minimal/RiftApp.hpp
@@ -158,7 +158,7 @@ protected:
 			const auto& vp = _sceneLayer.Viewport[eye];
 			glViewport(vp.Pos.x, vp.Pos.y, vp.Size.w, vp.Size.h);
 			_sceneLayer.RenderPose[eye] = eyePoses[eye];
-			renderScene(_eyeProjections[eye], ovr::toGlm(eyePoses[eye]));
+			renderScene(_eyeProjections[eye], ovr::toGlm(eyePoses[eye]), _eyeRenderDescs[eye]);
 		});
 		glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, 0, 0);
 		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
@@ -175,5 +175,5 @@ protected:
 		glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
 	}
 
-	virtual void renderScene(const glm::mat4& projection, const glm::mat4& eyePose) = 0;
+	virtual void renderScene(const glm::mat4& projection, const glm::mat4& eyePose, ovrEyeRenderDesc eyeRenderDesc) = 0;
 };
